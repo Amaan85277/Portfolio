645de37d554219b18141ef5a860e8e0eb7e7967d
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2024-03-09 22:30:44 +0530

    freeze screen after scroll function

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -8,20 +8,32 @@ import Five from "./Five";
 import Header from "../Header";
 import NavBar from "../NavBar";
 import { Context } from "../../Contexts";
+import { freezeScroll, freeScroll } from "../../utils/scrollUtils";
 
 function Home() {
   const [activeWindow, setActiveWindow] = useState(1);
 
   let timeout;
-  function scrollTo(i) {
+  let scroll_timeout;
+
+  async function scrollTo(i) {
+    freezeScroll(); //freeze scroll as soon as the function is initiated.
+
     clearTimeout(timeout);
-    timeout = setTimeout(() => {
+    clearTimeout(scroll_timeout);
+
+    timeout =  setTimeout(() => {
       setActiveWindow(i);
+
       const element = document.getElementById(`#${i}`);
       if (element) {
         element.scrollIntoView();
       }
-    }, 800);
+    }, 100);
+
+    scroll_timeout = setTimeout(() => {
+      freeScroll(); // free the scroll as soon as the function end...
+    }, 1500);
   }
 
   function scrollToWithoutLag(i) {
