36eb9bb323bb9ac4c7631284dac224b895ead975
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2024-03-12 17:32:45 +0530

    added landing page loading screen

---
+++ src/Components/LandingPage/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import Home from "../Home";
+import styles from "./styles.module.css";
+
+function LandingPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1200);
+  });
+
+  return (
+    <div>
+      {loading ? (
+        <div className={styles.container}>
+          <div className={styles.box} />
+        </div>
+      ) : (
+        <Home />
+      )}
+    </div>
+  );
+}
+
+export default LandingPage;
