8a161f5f9db5ff86a115649930eb5087bc4564a4
AmaanAhmadCogo <amaan.ahmad@cogoport.com>
2024-03-29 18:51:30 +0530

    added debounce to scroll

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -9,39 +9,29 @@ import Header from "../Header";
 import NavBar from "../NavBar";
 import { Context } from "../../Contexts";
 import { freezeScroll, freeScroll } from "../../utils/scrollUtils";
+import debounce from "lodash.debounce";
 
 function Home() {
   const [activeWindow, setActiveWindow] = useState(1);
 
-  let timeout;
   let scroll_timeout;
 
-  async function scrollTo(i) {
+  function scrollTo(i) {
     freezeScroll(); //freeze scroll as soon as the function is initiated.
-
-    clearTimeout(timeout);
     clearTimeout(scroll_timeout);
+    setActiveWindow(i);
 
-    //? the timeout here, for scrolling is added : 
-    //? because when the function 'scrollToWithoutLag' is triggered via navbar click,
-    //? if there is no timeout for the below 'timeout' function than 
-    //? even if the user is on 5th page and clicks 1st page nav index (diamond), 
-    //? it'll only go to the above index page i.e. 4th page
-    //? as the 'scrollToWithoutLag' function will be over-written by the useEffect under pages and trigger 'scrollTo'
-  
-    timeout =  setTimeout(() => {
-      setActiveWindow(i);
-      
-      const element = document.getElementById(`#${i}`);
-      if (element) {
-        element.scrollIntoView();
-      }
-    }, 280);
-    
+    const element = document.getElementById(`#${i}`);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+
     scroll_timeout = setTimeout(() => {
       freeScroll(); // free the scroll as soon as the function end...
     }, 1000);
   }
+
+  const scrollWithDebounce = debounce(scrollTo, 1000);
 
   function scrollToWithoutLag(i) {
     setActiveWindow(i);
@@ -70,11 +60,11 @@ function Home() {
         />
       )}
 
-      <One scrollTo={scrollTo} />
-      <Two scrollTo={scrollTo} />
-      <Three scrollTo={scrollTo} />
-      <Four scrollTo={scrollTo} />
-      <Five scrollTo={scrollTo} />
+      <One scrollTo={scrollWithDebounce} />
+      <Two scrollTo={scrollWithDebounce} />
+      <Three scrollTo={scrollWithDebounce} />
+      <Four scrollTo={scrollWithDebounce} />
+      <Five scrollTo={scrollWithDebounce} />
     </div>
   );
 }
