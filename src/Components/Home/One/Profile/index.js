856e005415801904e229c2cda82e27179d8694d1
AmaanAhmadCogo <amaan85277@gmail.com>
2024-05-02 18:20:07 +0530

    changed resume, added laazy loading for profile pic

--- src/Components/Home/One/Profile/index.js
+++ src/Components/Home/One/Profile/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from './styles.module.css'
-import ProfilePic from '../../../../assets/ProfilePic.png';
+import React from "react";
+import styles from "./styles.module.css";
+import ProfilePic from "../../../../assets/ProfilePic.png";
 
 function Profile() {
   return (
-      <div className={styles.container}>
-          <span className={styles.img_wrapper}>
-              <img src={ProfilePic} alt='profile' />
-          </span>
+    <div className={styles.container}>
+      <span className={styles.img_wrapper}>
+        <img src={ProfilePic} loading="lazy" alt="profile" />
+      </span>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
-import resume from "../../assets/resume.pdf";
 import { IoBackspace } from "react-icons/io5";
+import resume from "../../assets/resume.pdf";
 
 function Resume() {
   return (
     <div className={styles.container}>
-      <Link to="/"><IoBackspace className={styles.back} /></Link>
+      <Link to="/">
+        <IoBackspace className={styles.back} />
+      </Link>
 
       <div className={styles.pdf_container}>
         <iframe title="my-resume" src={resume} className={styles.pdf} />
